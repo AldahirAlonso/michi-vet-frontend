44c6064e879b9a8c1e054b347215b0f0e6425c8e
Aldahir Alonso <aldahir0603@hotmail.com>
2026-03-30 00:07:35 -0600

    Inicio de Proyecto, Instalacion de Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className="p-82 bg-gradient-to-r from-purple-400 via-pink-400 to-pink-300">
+      <h1 className="text-6xl text-white font-light text-center ">Michi Vet Cuernavaca</h1>
+      <h2 className="text-5xl text-white font-light text-center mt-5">Veterinaria Especial para Michis</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
