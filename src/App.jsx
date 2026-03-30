b8124ac233184fb365dc948b26d89f3038d4d908
Aldahir Alonso <aldahir0603@hotmail.com>
2026-03-30 15:09:02 -0600

    Importando Componentes: Header y Hero.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Header from "./components/Header.jsx"
+import Hero from "./components/Hero.jsx"
+
 function App() {
 
   return (
     <>
-      <div className="p-82 bg-gradient-to-r from-purple-400 via-pink-400 to-pink-300">
-      <h1 className="text-6xl text-white font-light text-center ">Michi Vet Cuernavaca</h1>
-      <h2 className="text-5xl text-white font-light text-center mt-5">Veterinaria Especial para Michis</h2>
-      </div>
+      <Header />
+      <Hero />
     </>
   )
 }
