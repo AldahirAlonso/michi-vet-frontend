2a4d08ec5e4eb5dd07e4a781b042c719301c4071
Aldahir Alonso <aldahir0603@hotmail.com>
2026-03-31 13:10:54 -0600

    Centrando Header con mx-auto y boton contacto terminado.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { Cat } from 'lucide-react';
 function Header() {
     return(
         // Contenedor principal del encabezado: define fondo, padding y sombra para separar visualmente la barra superior.
-        <header className="bg-white p-4 shadow-xl">
+        <header className="bg-white p-4 shadow-xl sticky">
 
             {/* Limita el ancho del contenido para que no ocupe todo el viewport en pantallas grandes. */}
-            <div className="container">
+            <div className="mx-auto container">
 
                 {/* Navegacion horizontal: alinea logo a la izquierda y enlaces a la derecha con flex. */}
                 <nav className="p-0 flex items-center justify-between">
@@ -21,10 +21,18 @@ function Header() {
 
                     {/* Lista de enlaces internos que desplazan a secciones de la pagina y aplican color hover para feedback visual. */}
                     <ul className="text-lg flex justify-end space-x-8 font-semibold">
-                        <a href="#inicio" className="text-black hover:text-pink-400">Inicio</a>
-                        <a href="#servicios" className="text-black hover:text-pink-400">Servicio</a>
-                        <a href="#sobre nosotros" className="text-black hover:text-pink-400">Sobre Nosotros</a>
-                        <a href="#contacto" className="bg-purple-400">Contacto</a>
+                        <li>
+                            <a href="#inicio" className="text-black hover:text-pink-400">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="#servicios" className="text-black hover:text-pink-400">Servicio</a>
+                        </li>
+                        <li>
+                            <a href="#sobre nosotros" className="text-black hover:text-pink-400">Sobre Nosotros</a>
+                        </li>
+                        <li>
+                            <a href="#contacto" className="bg-purple-500 text-white px-5 py-2 rounded-full hover:bg-purple-600 transition-all shadow-md hover:shadow-lg active:scale-95">Contacto</a>
+                        </li>
                     </ul>
                 </nav>
             </div>
