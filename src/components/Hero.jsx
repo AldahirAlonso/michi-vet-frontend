dd96585520eaa3d56d358fe2fdaa6e84c32fd2ca
Aldahir Alonso <aldahir0603@hotmail.com>
2026-04-05 13:02:23 -0600

    Ajuste de layout en contenedor con flexbox.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,30 +2,35 @@ import { Calendar } from 'lucide-react';
 
 function Hero() {
     return (
-        <section className="bg-gray-100 py-20">
-            <div>
-                <h1 className="text-5xl text-blue-500 text-center font-extrabold mb-6">
-                    El cuidado que tu <span className="text-pink-400">michi</span> merece
-                </h1>
-                <p className="text-lg text-gray-600 text-center font-light mx-auto mb-8">En MichiVet somos especialistas apasionados por la salud felina.
-                    Brindamos atención médica de alta calidad en un ambiente diseñado solo para gatos.
-                </p>
-                <div className="flex justify-center">
+        <section className="relative bg-white-100 py-16 lg:py-24 overflow-hidden">
+            <div className="container mx-auto px-4 flex flex-col lg:flex-row items-center">
 
-                    <a href="#contacto" className="bg-blue-400 text-white px-5 py-3 rounded-full hover:bg-blue-500 transition-all shadow-md hover:shadow-lg active:scale-95 flex items-center">
-                        <Calendar size={32} className="text-black w-10 h-8" />
-                        Agenda tu cita
-                    </a>
+                <div className="lg:w-1/2 text-center lg:text-left z-10">
+                    <h1 className="text-6xl font-extrabold text-purple-500 leading-tight mb-6">
+                        El cuidado que tu <span className="text-pink-400">michi</span> merece
+                    </h1>
+
+                    <p className="text-lg text-gray-600 font-light mx-auto mb-8 max-w-lg lg:mx-0">En MichiVet somos especialistas apasionados por la salud felina.
+                        Brindamos atención médica de alta calidad en un ambiente diseñado solo para gatos.
+                    </p>
+                    <div className="flex flex-col sm:flex-row justify-center lg:justify-start gap-4">
+                        <button className="bg-purple-400 text-white px-8 py-3 rounded-full hover:bg-purple-500 transition-all shadow-md hover:shadow-lg active:scale-95">
+                            <Calendar size={32} className="text-black w-10 h-8" />
+                            Agenda tu cita
+                        </button>
+                    </div>
                 </div>
 
-                <div className={"mt-12 flex justify-center"}>
-
-                    <img src="/images/hero-cat.png" alt="Imagen de un gato feliz" className="w-96 h-auto rounded-lg shadow-lg" />
-
+                <div className="lg:w-1/2 mt-12 flex flex-col justify-center relative gap-4">
+                    <div className=" gap-4"></div>
+                    <img
+                        src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?auto=format&fit=crop&q=80&w=600"
+                        alt="Imagen de un gato feliz"
+                        className="relative z-10 rounded-2xl w-full max-w-md object-cover transform rotate-2 hover:rotate-0 transition-transform duration-500"
+                    />
                 </div>
+
             </div>
-
-
         </section>
     )
 }
