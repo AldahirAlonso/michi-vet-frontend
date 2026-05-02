a2e65761504e16afcdbbb559a2ed7fdf91cbfe26
Aldahir <aldahir0603@hotmail.com>
2026-05-01 23:28:38 -0600

    Creando seccion contacto.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+function Contact() {
+    return (
+        <section className="bg-blue-300 p-20 lg:p-28">
+            <div className="container mx-auto flex justify-center">
+                    <h2 className="bg-pink-300 text-4xl font-bold text-center rounded-full py-4 px-2">Contacto</h2>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
