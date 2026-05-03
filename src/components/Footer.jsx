4a26c3369c8cd061b52cd38f6188e95b166c76e7
Aldahir <aldahir0603@hotmail.com>
2026-05-03 13:17:30 -0600

    Crear seccion Footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer className="bg-purple-600 mx-auto p-12">
+            <div className="px-20 flex flex-row justify-between items-center text-2xl font-semibold text-white">
+                <p>Michivet</p>
+                <p>© 2026 Todos los derechos reservados.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
